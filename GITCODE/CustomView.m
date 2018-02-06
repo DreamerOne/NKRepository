4f031be7a28072fdef8f83db981909331b0e746e
YLTAPPleDeveloper <1958070022@qq.com>
2018-02-06 14:52:27 +0800

    add a customView

---
+++ GITCODE/CustomView.m
@@ -0,0 +1,21 @@
+//
+//  CustomView.m
+//  GITCODE
+//
+//  Created by doctor on 2018/2/6.
+//  Copyright © 2018年 doctor. All rights reserved.
+//
+
+#import "CustomView.h"
+
+@implementation CustomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

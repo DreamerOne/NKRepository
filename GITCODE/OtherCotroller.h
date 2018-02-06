7d47493e141a6b061140e16d35c425dd5470003e
YLTAPPleDeveloper <1958070022@qq.com>
2018-02-06 11:11:16 +0800

    创建了一个控制器

---
+++ GITCODE/OtherCotroller.h
@@ -0,0 +1,13 @@
+//
+//  OtherCotroller.h
+//  GITCODE
+//
+//  Created by doctor on 2018/2/6.
+//  Copyright © 2018年 doctor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OtherCotroller : UIView
+
+@end
---
+++ GITCODE/OtherCotroller.m
@@ -0,0 +1,21 @@
+//
+//  OtherCotroller.m
+//  GITCODE
+//
+//  Created by doctor on 2018/2/6.
+//  Copyright © 2018年 doctor. All rights reserved.
+//
+
+#import "OtherCotroller.h"
+
+@implementation OtherCotroller
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

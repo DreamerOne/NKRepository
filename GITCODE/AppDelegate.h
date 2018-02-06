5b67ed66df63a68c9e8e3de65a7680ae1fe8e111
YLTAPPleDeveloper <1958070022@qq.com>
2018-02-06 11:04:27 +0800

    第一次修改

---
+++ GITCODE/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GITCODE
+//
+//  Created by doctor on 2018/2/6.
+//  Copyright © 2018年 doctor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GITCODE/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GITCODE
+//
+//  Created by doctor on 2018/2/6.
+//  Copyright © 2018年 doctor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
